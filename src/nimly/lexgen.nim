5f401f367dabf35275e4b1a38f9ab84ab29ad55e
Lolo Iccl <oxisccl@gmail.com>
2019-02-28 01:31:14 +0900

    add makeDFA

--- src/nimly/lexgen.nim
+++ src/nimly/lexgen.nim
@@ -10,11 +10,22 @@ proc `~`[A](a: A): ref A =
 
 
 type
+  # for SynTree
   Pos = int16
   BOp = enum
     bor,
     bcat
   Pos2PosSet = TableRef[Pos, set[Pos]]
+
+  # for DFA
+  DState = int16
+  DTranRow = TableRef[char, DState]
+  DTran = TableRef[DState, DTranRow]
+  DFA = object
+    start: DState
+    accepts: set[DState]
+    states: set[DState]
+    tran: DTran
 
 variant LChar:
   End
@@ -28,7 +39,6 @@ variant ReSynTree:
   Term(lit: Lit)
   Bin(op: BOp, left: ref ReSynTree, right: ref ReSynTree)
   Star(child: ref ReSynTree)
-
 
 proc collectPos(t: ReSynTree): set[Pos] =
   # error on tree with not unique positions when debug
@@ -47,6 +57,23 @@ proc collectPos(t: ReSynTree): set[Pos] =
       return lr + rr
     Star(child: c):
       return c[].collectPos
+
+proc collectChar(t: ReSynTree): set[char] =
+  match t:
+    Term(lit: l):
+      match l:
+        Empty:
+          return {}
+        Char(pos: p, c: lc):
+          match lc:
+            End:
+              return {}
+            Real(c: c):
+              return {c}
+    Bin(op: _, left: l, right: r):
+      return l[].collectChar + r[].collectChar
+    Star(child: c):
+      return c[].collectChar
 
 proc nullable(t: ReSynTree): bool =
   match t:
@@ -127,3 +154,114 @@ proc makeFollowposTable(t: ReSynTree): Pos2PosSet =
       for i in t.lastpos:
         result[i] = t.firstpos
       result.mergeSetTable(c[].makeFollowposTable)
+
+proc terms(t: ReSynTree): seq[Lit] =
+  match t:
+    Term(lit: l):
+      return @[l]
+    Bin(op: _, left: l, right: r):
+      return l[].terms & r[].terms
+    Star(child: c):
+      return c[].terms
+
+proc makePosCharTable(t: ReSynTree): TableRef[Pos, char] =
+  result = newTable[Pos, char]()
+
+  for l in t.terms:
+    match l:
+      Char(pos: p, c: l):
+        match l:
+          Real(c: c):
+            result[p] = c
+          End:
+            continue
+      Empty:
+        continue
+
+proc makeCharPossetTable(t: ReSynTree): TableRef[char, set[Pos]] =
+  # init
+  let
+    chars = t.collectChar
+
+  result = newTable[char, set[Pos]]()
+  for c in chars:
+    result[c] = {}
+
+  for l in t.terms:
+    match l:
+      Char(pos: p, c: l):
+        match l:
+          Real(c: c):
+            result[c].incl(p)
+          End:
+            continue
+      Empty:
+        continue
+
+proc getAccPos(t: ReSynTree): Pos =
+  let chars = t.collectChar
+  for l in t.terms:
+    match l:
+      Char(pos: p, c: l):
+        match l:
+          Real:
+            continue
+          End:
+            return p
+      Empty:
+        continue
+  assert false
+
+proc stateNum(d: DFA): int =
+  return d.states.card
+
+proc makeDFA(t: ReSynTree): DFA =
+  let
+    followpos = t.makeFollowposTable
+
+  var
+    tran = newTable[DState, DTranRow]()
+    states: set[DState] = {}
+    posS2DState = newTable[set[Pos], DSTate]()
+    unmarked: seq[set[Pos]] = @[]
+
+  # init
+  let
+    chars = t.collectChar
+    iState = DState(states.card)
+    iSPos = t.firstpos
+    charPosset = t.makeCharPossetTable
+  states.incl(iState)
+  posS2DState[iSPos] = iState
+  unmarked.add(iSPos)
+
+  # make state and tran
+  while unmarked.len > 0:
+    let
+      ps = unmarked.pop
+      s = posS2DState[ps]
+    tran[s] = newTable[char, DState]()
+    for c in chars:
+      let posSet = ps * charPosset[c]
+      var newSPos: set[Pos] = {}
+      for p in posSet:
+        newSPos = newSPos + followpos[p]
+      var nState: DState
+      if posS2DState.hasKey(newSPos):
+        nState = posS2DState[newSPos]
+      else:
+        nState = DState(states.card)
+        unmarked.add(newSPos)
+        states.incl(nState)
+        posS2DState[newSPos] = nState
+      tran[s][c] = nState
+
+  # make accepts
+  let acc = t.getAccPos
+  var accepts: set[DState] = {}
+  for k in posS2DState.keys:
+    if acc in k:
+      accepts.incl(posS2DState[k])
+
+  # make DFA
+  return DFA(start: iState, accepts: accepts, states: states, tran: tran)
--- tests/test_lexgen.nim
+++ tests/test_lexgen.nim
@@ -2,21 +2,22 @@ import unittest
 
 include nimly/lexgen
 
-test "makeFollowposTable (Dragonbook 3.9.4)":
-  let
-    example = Bin(bcat,
-                  ~Bin(bcat,
+let dbookExample = Bin(bcat,
                        ~Bin(bcat,
                             ~Bin(bcat,
-                                 ~Star(~Bin(bor,
-                                            ~Term(Char(1, Real('a'))),
-                                            ~Term(Char(2, Real('b'))))
-                                 ),
-                                 ~Term(Char(3, Real('a')))),
-                            ~Term(Char(4, Real('b')))),
-                       ~Term(Char(5, Real('b')))),
-                  ~Term(Char(6, End())))
+                                 ~Bin(bcat,
+                                      ~Star(~Bin(bor,
+                                                 ~Term(Char(1, Real('a'))),
+                                                 ~Term(Char(2, Real('b'))))
+                                      ),
+                                      ~Term(Char(3, Real('a')))),
+                                 ~Term(Char(4, Real('b')))),
+                            ~Term(Char(5, Real('b')))),
+                       ~Term(Char(6, End())))
 
+
+test "test makeFollowposTable (Dragonbook 3.9.4)":
+  let
     followpos: array[0..4, (Pos, set[Pos])] = {
       Pos(1): {Pos(1), Pos(2), Pos(3)},
       Pos(2): {Pos(1), Pos(2), Pos(3)},
@@ -25,4 +26,20 @@ test "makeFollowposTable (Dragonbook 3.9.4)":
       Pos(5): {Pos(6)}
                 }
 
-  check example.makeFollowposTable == followpos.newTable
+  check dbookExample.makeFollowposTable == followpos.newTable
+
+test "test correctChar":
+  check dbookExample.collectChar == {'a', 'b'}
+
+test "test makeDFA (Dragonbook 3.9.5)":
+  let dfa = dbookExample.makeDFA
+  check dfa.states.card == 4
+  check dfa.tran[dfa.start]['b'] == dfa.start
+  let sa = dfa.tran[dfa.start]['a']
+  check dfa.tran[sa]['a'] == sa
+  let sab = dfa.tran[sa]['b']
+  check dfa.tran[sab]['a'] == sa
+  let acc = dfa.tran[sab]['b']
+  check dfa.accepts == {acc}
+  check dfa.tran[acc]['a'] == sa
+  check dfa.tran[acc]['b'] == dfa.start
