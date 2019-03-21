11eddebe51fff3f1907f9184e75f2b63beb988b0
Lolo Iccl <oxisccl@gmail.com>
2019-03-21 19:49:46 +0900

    revert unexpected chenge

--- tests/test_state_parse.nim
+++ tests/test_state_parse.nim
@@ -6,7 +6,7 @@ import state_parser
 
 test "test state":
   var lexer = testStateLex.newWithString("if test + 1 then { true } else { 2 * ( test + 3 ) }")
-  lexer.ignoreIf = proc(r: StateToken): bool = r.kind == StateTokenKind.SIGNOR
+  lexer.ignoreIf = proc(r: StateToken): bool = r.kind == StateTokenKind.SIGNORE
 
   testStatePar.initParser()
   check testStatePar.parse(lexer) == "IF(test+1)THEN{true}ELSE{(2*(test+3))}"
