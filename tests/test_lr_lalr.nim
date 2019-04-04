2da83f4ecda10079c2b4af7acb3006314b2ae7d4
Lolo Iccl <oxisccl@gmail.com>
2019-04-04 21:01:58 +0900

    mod test_lr_lalr

--- tests/test_lr_lalr.nim
+++ tests/test_lr_lalr.nim
@@ -7,7 +7,7 @@ import parser_415_lr
 ## to check LR:CONFLICT in -d:nimydebug mode
 
 test "test lalr":
-  var lexer415 = lex415.newWithString("*left")
+  var lexer415 = lex415.newWithString("left")
   lexer415.ignoreIf = proc(r: MyTerm): bool = r.kind == MyTermKind.IGNORE
   psr415LALR.init
   check parser_415.parse(psr415LALR, lexer415) == "left"
